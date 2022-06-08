af7f62d1a4078c3f059b299ed37f3011dc43357b
Leah Young <leah.young21@alumni.colostate.edu>
2022-06-07 18:26:18 -0600

    Add new variables

---
+++ variable.js
@@ -0,0 +1,20 @@
+// # Strings
+
+var email = "Thundercats@gmail.com"
+var firstName = "Henry"
+var lastName = "Thompson"
+var userName = "TripleThreatTina"
+
+// # Numbers
+
+var age = 30
+var population = 1000
+var previousBalance = 425.68
+
+// # Booleans
+
+var formSubmitted = true
+var engaged = false
+var tuitionPaid = true
+
+var firstName = "Karen"
